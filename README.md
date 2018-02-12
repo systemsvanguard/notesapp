ad5adc38fb84a6e05d442cae9905d7c2fabc84b2
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2018-02-12 04:27:04 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Note-taking App
+
+A simple but fun **Angular.js** and W3.css powered **_Notetaking App_**.
+
+![Note-taking App](http://ryanhunter.org/portfolio/notesapp/thumb_notesapp.jpg)
+
+---
+
+## Features
+- Angular.js framework
+- W3.css library from w3schools.com
+- Sweet Alerts library
+- Fontastic.me custom generated vector icons
+- Google Fonts
+
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
---
+++ notesapp/notesapp.js
@@ -0,0 +1,21 @@
+// Note Taking application.  Uses Angular.js 1.6.
+var NotesStored = "";
+
+var app = angular.module("notetakingApp", []); 
+app.controller("notetakingCtrl", function($scope) {
+    $scope.message = "";
+    $scope.charactersLeft = function() {
+        return 90 - $scope.message.length;
+    };
+    $scope.clear = function() {
+        $scope.message = "";
+    };
+    $scope.save = function() {
+        // Save then clear the message
+		NotesStored = NotesStored + $scope.message + "<br>";		
+		document.getElementById("notesArchived").innerHTML = NotesStored;
+		//  alert("Your note has been saved!");
+		swal("Saved", "Your note has been saved!", "success");
+		$scope.message = "";
+    };
+});
